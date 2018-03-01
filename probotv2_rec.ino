cb09d416569aead22b2cbae16992892fc213b69a
shubham jain <36927669+ProboticsX@users.noreply.github.com>
2018-03-01 14:34:19 +0530

    code for hand motion gesture controlled car using accelerometer and RF

---
+++ probotv2_rec.ino
@@ -0,0 +1,102 @@
+#include <VirtualWire.h>
+#include <AFMotor.h>
+
+AF_DCMotor motor1(3);
+AF_DCMotor motor2(2);
+const int led_pin = 6;
+const int transmit_pin = 12;
+const int receive_pin = 52;
+const int transmit_en_pin = 3;
+
+void setup()
+{
+  Serial.begin(9600);
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+ 
+  motor1.run(RELEASE);   
+  motor2.run(RELEASE);
+   
+    delay(1000);
+    Serial.begin(9600);  // Debugging only
+    Serial.println("setup");
+
+    // Initialise the IO and ISR
+    vw_set_tx_pin(transmit_pin);
+    vw_set_rx_pin(receive_pin);
+    vw_set_ptt_pin(transmit_en_pin);
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(2000);  // Bits per sec
+      vw_rx_start();       // Start the receiver PLL running
+
+   
+
+  
+}
+
+void left(){
+ motor1.run(FORWARD); 
+ motor2.run(RELEASE);
+}
+
+void right(){
+   motor1.run(RELEASE);
+   motor2.run(FORWARD);
+
+}
+
+void up(){
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+}
+
+void down(){
+  motor1.run(BACKWARD); 
+  motor2.run(BACKWARD);
+}
+
+void neutral(){
+   motor1.run(RELEASE);
+   motor2.run(RELEASE);
+
+}
+
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+     char a;
+  int i;// Serial.println("hi");
+  int y=0;
+  for (i = 0; i < buflen; i++)
+  {
+      
+         a=char(buf[i]);
+          Serial.println(a); 
+      }
+
+if(a=='l')
+left();
+
+if(a=='r')
+right();
+
+if(a=='u')
+up();
+
+if(a=='d')
+down();
+
+if(a=='n')
+neutral();
+
+        
+    }
+}
+
---
+++ probotv2_tra.ino
@@ -0,0 +1,75 @@
+// MPU-6050 Short Example Sketch
+// By Arduino User JohnChi
+// August 17, 2014
+// Public Domain
+
+
+//vcc-5v gnd-gnd scl-a5 sda-a4
+
+#include<Wire.h>
+#include <VirtualWire.h>
+
+const int led_pin = 11;
+const int transmit_pin = 12;
+const int receive_pin = 2;
+const int transmit_en_pin = 3;
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+  vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);   // Bits per sec
+  Serial.begin(9600);
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX); //delay(2000);
+ Serial.print("      AcY = "); Serial.println(AcY);
+
+char a[1];
+if(AcX<-10000)
+a[0]='d';
+
+if(AcX>10000)
+a[0]='u';
+
+if(AcY>10000)
+a[0]='l';
+
+if(AcY<-10000)
+a[0]='r';
+
+if(AcX<10000 && AcX>-10000 && AcY<10000 && AcY>-10000)
+a[0]='n';
+
+ vw_send((uint8_t *)a, 1);
+  vw_wait_tx(); // Wait until the whole message is gone
+
+ // delay(1000);
+ 
+/*  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(333);*/
+}
+
